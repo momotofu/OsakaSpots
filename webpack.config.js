665186a256c4d264543d76a9ac63c974f2e3870e
Christopher Reece <mrkita.reece@gmail.com>
2018-06-21 12:14:10 +0900

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = (env, options) => {
+    return {
+      entry: {
+          app: [
+            path.resolve(__dirname, './src/index.js')
+          ]
+      },
+      output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'http://localhost:2992/assets/',
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[id].[chunkhash].js'
+      },
+      resolve: {
+        extensions: ['.js', '.css']
+      },
+      devServer: {
+       headers: { "Access-Control-Allow-Origin": "*" }
+      },
+      module: {
+          rules: [
+              {
+                test: /\.js$/i, use: 'babel-loader',
+                exclude: /node_modules/
+              }
+          ]
+      },
+      plugins: [
+        new UglifyJsPlugin()
+      ]
+  }
+}
