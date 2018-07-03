55f4fe023812304aa7cc8be0944d65f782b7d041
Christopher Reece <mrkita.reece@gmail.com>
2018-07-03 19:59:18 +0900

    fleshed out fetchListings func

--- src/frontend/components/controlPanel/index.js
+++ src/frontend/components/controlPanel/index.js
@@ -10,16 +10,26 @@ const viewModel = function(params) {
   this.listings = ko.observableArray()
   this.init = function() {
     // check if listings are in user's local storage
-    if (!localStorage.osakaSpots && !localStorage.osakaSpots.listings) {
-      localStorage.osakaSpots = JSON.stringify({})
-      localStorage.osakaSpots.listings = JSON.stringify([
-        // create a dummy listing while server data is fetched
-        new Listing('pin', 'Hatsushiba Station', '{lat: 0, lng: 0}', 'station')
-      ])
+    if (!localStorage.osakaSpots || !JSON.parse(localStorage.osakaSpots).hasOwnProperty('listings')) {
+      localStorage.osakaSpots = JSON.stringify({
+        listings: [
+          // create a dummy listing while server data is fetched
+          new Listing('pin', 'Hatsushiba Station', '{lat: 0, lng: 0}', 'station')
+        ]
+      })
 
+      // fetch listings from server. On success update listings observableArray
       this.fetchListings()
+        .then(listings => {
+          this.listings(listings)
+          localStorage.osakaSpots = JSON.stringify({ listings })
+        })
+        .catch(console.error)
+
     } else {
-      this.listings(JSON.parse(localStorage.osakaSpots.listings))
+      // if listings is already stored locally, then populate listings
+      // observable array with local data.
+      this.listings(JSON.parse(localStorage.osakaSpots).listings)
     }
   }
 
@@ -28,7 +38,7 @@ const viewModel = function(params) {
     const url = '/listings'
 
     return await $.getJSON(url, function(data) {
-      return JSON.parse(data).map((graph) => {
+      return data.map((graph) => {
         return new Listing(graph)
       })
     })
