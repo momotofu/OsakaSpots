2e069eeb47b0cd361585098cec8e4fe56706317f
Christopher Reece <mrkita.reece@gmail.com>
2018-07-11 09:34:12 +0900

    added a custom input cursor

--- src/frontend/components/listingSearchBar/index.js
+++ src/frontend/components/listingSearchBar/index.js
@@ -6,6 +6,12 @@ import './styles'
 const template = listingSearchbar()
 const viewModel = function(params) {
   //////////////////////////////////////////////////////////////////////////////
+  // constants
+  //////////////////////////////////////////////////////////////////////////////
+
+  const inputCursor = document.getElementsByClassName('listingSearchbar__cursor')[0]
+
+  //////////////////////////////////////////////////////////////////////////////
   // observables
   //////////////////////////////////////////////////////////////////////////////
 
@@ -21,7 +27,7 @@ const viewModel = function(params) {
   this.searchInputChange = (viewModel, event) => {
     event.preventDefault()
     this.inputData({
-      searchString: event.data,
+      searchString: event.target.value,
       enterPressed: false
     })
   }
@@ -30,11 +36,49 @@ const viewModel = function(params) {
   this.searchInputEnter = (viewModel, event) => {
     event.preventDefault()
     this.inputData({
-      searchString: event.data,
+      searchString: event.target.value,
       enterPressed: true
     })
 
   }
+
+  // cursor logic
+  var cursorIsHidden = false
+
+  window.setInterval(() => {
+    cursorIsHidden = !cursorIsHidden
+
+    if (cursorIsHidden)
+      inputCursor.style.opacity = 1
+    else
+      inputCursor.style.opacity = 0
+
+  }, 700)
+
+  const updateCursor = (length) => {
+    if (length === 1)
+      inputCursor.style.marginLeft = '12px'
+    else
+      inputCursor.style.marginLeft = `${length + 18}px`
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // subscriptions
+  //////////////////////////////////////////////////////////////////////////////
+
+  this.inputData.subscribe((inputData) => {
+    // measure the pixel width if the searchString
+    const s = document.createElement('div')
+    s.style.fontSize = '24px'
+    s.style.position = 'absolute'
+    s.style.visibility = 'hidden'
+    s.innerText = inputData.searchString
+    document.body.appendChild(s)
+    const pixelWidth = s.offsetWidth + 1
+    document.body.removeChild(s)
+
+    updateCursor(pixelWidth)
+  })
 
   // keep this function at the bottom of this class
   params.callback(this)
